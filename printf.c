61234709871844cf1acfe0f9a8b5471b2fa09dd9
lucenara <107366057+lucenara@users.noreply.github.com>
2025-02-08 15:07:15 +0000

    new file:   .vscode/tasks.json 	modified:   README.md 	new file:   printf 	new file:   printf.c

---
+++ printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+    int main(){
+        int idade = 31;
+        float altura = 1.64;
+        double saldoBancario = 12345.76;
+        char inicial = 'R';
+        char nome[20] = "Rafaella";
+
+        printf("Idade: %d anos\n", idade);
+        printf("Altura: %.2f metros\n", altura);
+        printf("Saldo Bancário: %.2f reais\n", saldoBancario);
+        printf("Inicial do nome: %c\n", inicial);
+        printf("Nome: %s\n", nome);
+
+
+    }   
